a94de1fc6b43a87abb279d779168bf96ce2a70a9
Bumpercarz6 <daniel.gale@shaw.ca>
2026-02-07 19:44:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,11 @@ const client = new Client({
  * GOOGLE AUTH
  ********************/
 const auth = new google.auth.GoogleAuth({
-  keyFile: "credentials.json",
-  scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  },
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
 const sheets = google.sheets({ version: "v4", auth });
